b660d0c1aaaaddace7b9f64cb9d0c0d0efcd8160
armash <armashfankar@gmail.com>
2016-02-13 18:35:28 +0530

    abt us page

--- HTML/about-us.php
+++ HTML/about-us.php
@@ -17,7 +17,7 @@
 				<div class="col-md-6 tp-about-block"><!-- tp about block -->
 					<h1>Who We Are</h1>
 					<p class="lead">Manufacturer & Stockist: M.S. SEAMLESS, BOILERS HYDRAULIC, ERW, PIPES FITTINGS, FLANGES & TUBES Etc.</p>
-					<p>We are a leading manufacturer & Stockist , Supplier of Broad Array of quality pipe fittings for projects and engineering services needs in diverse industry verticals such as Oil & Gas, Petrochemical, Power conventional as well as nuclear, Steel Plants, Textile, Shipping & all other process industries</p>
+					<p style="font-size:16px;color:#afafaf;">We are a leading manufacturer & Stockist , Supplier of Broad Array of quality pipe fittings for projects and engineering services needs in diverse industry verticals such as Oil & Gas, Petrochemical, Power conventional as well as nuclear, Steel Plants, Textile, Shipping & all other process industries</p>
 				</div><!-- /.tp about block -->
 				<div class="col-md-6 tp-about-pic"><!-- tp about pic -->
 					<img src="images/abt.jpg" style="width:500px;" class="img-responsive" alt="hancopipefittings.com">
@@ -26,53 +26,27 @@
 			<div class="row tp-why-us">	<!-- tp why us -->
 				<div class="col-md-12 section-title"><!-- section title -->
           <h1>Why Choose Us</h1>
-					<p>Lorem ipsum dolol standards in the productio orem imet, consectetur adipisicing elit, do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolol et nop standards in the productio Nam vitae massa sceleri sque.</p>
+					<p style="font-size:16px;color:#afafaf;">All the offered products are made available in different grades and dimensions so as to cater to the exact needs of our valued patrons. Owing to the features like accurate dimensions, sturdy construction, high torque strength, corrosion & abrasion resistance and smooth edges, our entire offered range is widely popular among the clients, located across the globe. Moreover, possessing this range, we are catering to the demands of fertilizers, pharmacy, chemical, food & beverages, oil & gas, sugar and textile industries.</p>
 				</div><!-- /.section title -->
 				<div class="col-md-4"><!-- why block-->
           <div class="color-box"><!-- color box-->
 					<h2><i class="fa fa-cogs"></i>Technology</h2>
-					<p>Lorem ipsum dolol standards in the productio orem imet, consectetur adipisicing elit, do eiusmod tempor incididu aliqua.</p>
-					<ul class="angle-double-right">
-						<li>Lorem ipsum dolor sit starndard</li>
-						<li>Lorem ipsum dolor sit starndard</li>
-					</ul>
+					<p>We are a leading manufacturer & Stockist , Supplier of Broad Array of quality pipe fittings for projects and engineering services needs in diverse industry verticals such as Oil & Gas, Petrochemical, Power conventional as well as nuclear, Steel Plants, Textile, Shipping & all other process industries.</p>
 				</div><!-- /.color box-->
         </div><!-- /.why block-->
 				<div class="col-md-4"><!-- why block-->
           <div class="color-box"><!-- color box -->
 					<h2><i class="fa fa-dashboard"></i> Perfomance</h2>
-					<p>Lorem ipsum dolol standards in the productio orem imet, consectetur adipisicing elit, do eiusmod tempor incididu aliqua.</p>
-					<ul class="angle-double-right">
-						<li>Lorem ipsum dolor sit starndard</li>
-						<li>Lorem ipsum dolor sit starndard</li>
-					</ul>
+					<p>Hanco Pipe & Fitting specializes in exclusive products and specialized services like Quick delivery, in-house testing, etc. At Hanco Pipe & Fitting, all the finished products undergo quality check in house before inspection by the client or clients authorized representative prior to the delivery of fittings.</p>
 				</div><!-- /.color box -->
         </div><!-- /.why block-->
 				<div class="col-md-4"><!-- why block-->
           <div class="color-box"><!-- color box -->
-					<h2><i class="fa fa-lightbulb-o"></i> Perfomance</h2>
-					<p>Lorem ipsum dolol standards in the productio orem imet, consectetur adipisicing elit, do eiusmod tempor incididu aliqua.</p>
-					<ul class="angle-double-right">
-						<li>Lorem ipsum dolor sit starndard</li>
-						<li>Lorem ipsum dolor sit starndard</li>
-					</ul>
+					<h2><i class="fa fa-users"></i>Customers</h2>
+					<p>Over the years, we have always remained focused on our customers, developing our products to meet new challenges and support the market’s changing needs. At Hanco Pipe & Fitting, We aim at providing customer specific requirements by ensuring supply of quality products and services at competitive prices.</p>					
 				</div><!-- /.color box -->
         </div><!-- /.why block-->
 			</div><!-- /.tp why us -->
-			<div class="row tp-thumb-box"><!-- thumb box -->
-				<div class="col-md-6 thumb-block"><!-- thumb block -->
-					<img src="images/about-blk-pic.jpg" class="img-responsive" alt="">
-					<div class="tp-blog-info"><h2>A Histroy of Indus</h2>
-					<p>Lorem ipsum dolol standards in the productio orem imet, consectetur adipisicing elit, do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolol et nop standards in the productio  porta ex scelerisquecongue arcu. </p>
-					<a href="about-histroy.html" class="btn-outline">read more</a></div>
-				</div><!-- /.thumb block -->
-				<div class="col-md-6 thumb-block"><!-- thumb block -->
-					<img src="images/about-blk-pic1.jpg" class="img-responsive" alt="">
-					<div class="tp-blog-info"><h2>Mission &amp; Vision</h2>
-					<p>Lorem ipsum dolol standards in the productio orem imet, consectetur adipisicing elit, do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolol et nop standards in the productio  porta ex scelerisquecongue arcu. </p>
-					<a href="mission.html" class="btn-outline">read more</a></div>
-				</div><!-- /.thumb block -->
-			</div><!-- /.thumb box -->
 		</div>
 	</div><!-- /.tp-about -->
 </div><!-- /.main-wrapper -->
